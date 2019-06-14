e0c27b2243d8cdf63b2af8000c69aa8d7b39841f
dudelo <carlovega20@gmail.com>
2019-06-14 11:37:41 -0700

    main and classes

---
+++ classes.py
@@ -0,0 +1,48 @@
+import datetime
+
+
+#this is used for storing a list of tasks as well as adding them
+class tasks:
+
+    def __init__(self):                                           #constructor
+        pass
+    
+    def add_task(self, task_name, time_due):                      #adds a task with information passed into the parameters
+
+        task_to_add = task()
+        task_to_add.edit_task(task_name, time_due)
+        self.tasks_list.append(task_to_add)
+
+    def display_tasks(self):                                      #displays all of the tasks and their information
+
+        for task in self.tasks_list:
+            task.display_task()
+
+    tasks_list = []                                               #the tasks list which holds an array of tasks
+
+
+#a task class which holds information about it's name as well as it's due date
+class task:
+
+    def __init__(self):                                           #constructor
+        pass
+
+    def edit_task(self, name, due_date):                          #calls the edit_name and edit_due_date functions with parameters passed in
+
+        self.edit_name(name)
+        self.edit_due_date(due_date)
+
+    def edit_name(self, name_add):                                #edits the string name of the task and changes it to the name_add passed in
+
+        name = name_add
+
+    def edit_due_date(self, date_add):                            #edits the due date of the task which is a datetime object
+
+        due_date = date_add
+    
+    def display_task(self):                                       #displays the task
+        print(self.name)
+        print(self.due_date)
+
+    name = ""                                                     #name of the task
+    due_date = ""                                                   #datetime object of when it's due
---
+++ main.py
@@ -0,0 +1,22 @@
+import classes
+
+user_tasks = classes.tasks()
+
+print("hello, welcome to le task scheduling program!")
+
+while True:
+    print("1) add task\n2) display tasks\n3) quit")
+    user_input = input('>')
+
+    if(user_input == 1):
+        task_name = input('please enter in a task name: ')
+        due_date = input('now make up a due date: ')
+        user_tasks.add_task(task_name, due_date)
+
+    elif(user_input == 2):
+        user_tasks.display_tasks()
+
+    else:
+        break
+
+print('program finished')
