9ace1de7f787f522012b5b892b88d749dd291a24
Thomas Eykamp <embergameslp@gmail.com>
2019-06-14 10:56:52 -0700

    Changed LOG output format

--- logger.py
+++ logger.py
@@ -6,6 +6,6 @@ location = os.path.dirname(os.path.abspath(__file__))
 
 def log(message):
     # creates log file
-    logger = os.path.join(location, "Log.hdf")
+    logger = os.path.join(location, "Log.txt")
     with open(logger, "w+") as handle:
         print(str(time.ctime()) + " >>> " + str(message), file=handle)
