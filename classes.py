4266ba8469d7dcf13592f13722f8de87ea9f35d7
dudelo <carlovega20@gmail.com>
2019-06-14 19:27:44 -0700

    edit task function changed

--- classes.py
+++ classes.py
@@ -8,6 +8,7 @@ import logger                                                   # see logger mod
 class User_tasks(object):
 
     def __init__(self):                                           #constructor
+
         self.tasks_list = []                                      #the tasks list which holds an array of tasks
         logger.log("User_Tasks Created")
 
@@ -18,7 +19,6 @@ class User_tasks(object):
 
         logger.log("Adding Task")
 
-
     def display_tasks(self):                                      #displays all of the tasks and their information
 
         for task in self.tasks_list:
@@ -26,20 +26,18 @@ class User_tasks(object):
 
         logger.log("Displaying Tasks")
     
-    def edit_task(self):                         # calls the edit_name and edit_due_date functions with parameters passed in
-        selected_name = input("Which task do you want to change?")
-        edit = input("Do you want to rename or change the due date?")
-        if edit.lower().strip() == "rename":
-            rename = input("What is the new name?")
-            selected_name.edit_name(rename)
+    def edit_task(self, task_name, name_change, date_change):     # calls the edit_name and edit_due_date functions with parameters passed in
 
-            logger.log("Renamed Task")
+        for task in self.tasks_list:
+            if(task.name == task_name):
 
-        else:
-            date = input("When is it due?")
-            selected_name.edit_due_date(date)
+                if(name_change):
+                    task.edit_name(name_change)
+                    return
 
-            logger.log("Changed Due Date")
+                if(date_change):
+                    task.edit_due_date(date_change)
+                    return
 
 
 #a task class which holds information about it's name as well as it's due date
@@ -48,7 +46,7 @@ class Task(object):
     def __init__(self, name="Untitled", due_date="Jan 1, 2099"):                                           #constructor
 
         self.name = name                                          #name of the task
-        self.due_date = Timer(due_date)                                 #datetime object of when it's due
+        self.due_date = Timer(due_date)                           #datetime object of when it's due
 
     def edit_name(self, new_name):                                #edits the string name of the task and changes it to the name_add passed in
         self.name = new_name
@@ -57,6 +55,7 @@ class Task(object):
         self.due_date = new_date
     
     def display_task(self):                                       #displays the task -- This will be eventually passed to the display class
+
         print(self.name)
         print(self.due_date)
 
@@ -67,6 +66,7 @@ class Timer(object):
         self.date = date
 
     def date_diff(self):
+
         dt = datetime.datetime
         now = dt.now()
         return dt(year=self.date, month=self.date, day=self.date) - dt(year=now.year, month=now.month, day=now.day, minute=now.minute) # will need a way to differentiate days, hours, minutes
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import logger
 logger.start()
 user_tasks = classes.User_tasks()
 
-print("hello, le task scheduling program has arrived!")         #      #meme
+print("hello, le task scheduling program has arrived! this'll be ebic")         #is this so ppl who view this understand its a meme? ----->      #meme
 
 while True:
-    print("1) add task \n2) display tasks \n3) quit")
+    print("1) add task \n2) display tasks \n3) edit task\n4) quit")
     user_input = int(input('>'))
 
     if user_input == 1:
@@ -17,13 +17,37 @@ while True:
         due_date = input('now make up a due date: \n>')
         user_tasks.add_task(task_name, due_date)
         
-
     elif user_input == 2:
         logger.log("User Display Task")
 
         user_tasks.display_tasks()
 
+    elif user_input == 3:
+        logger.log("User editing task")
+        user_tasks.display_tasks()
+
+        to_edit = input('please enter in the name of the task that you want to edit: \n>')
+        edit_selection = int(input('1) rename task \n2) change due date \n3) cancel: \n>'))
+
+        if(edit_selection == 1):
+            name_change = input('enter name change: \n>')
+            user_tasks.edit_task(to_edit, name_change, None)
+
+            logger.log("name changed")
+
+        elif(edit_selection == 2):
+            date_chage = input('enter date change: \n>')
+            user_tasks.edit_task(to_edit, None, date_chage)
+
+            logger.log("date changed")
+
+        else:
+            break
+
+
+
     else:
+        print('quitting')
         break
 
 logger.log("End--------------------------------------------------")
