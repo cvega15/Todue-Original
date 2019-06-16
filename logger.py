ff412dcb77ac3027a5fd5e6e3109722d1162f0a5
Thomas Eykamp <embergameslp@gmail.com>
2019-06-16 09:08:04 -0700

    Logger Housekeeping

--- logger.py
+++ logger.py
@@ -1,17 +1,17 @@
 import time
 import os
 
-
+# finds the location of the executed py file and sets this as the destination for the Log.txt
 location = os.path.dirname(os.path.abspath(__file__))
 def start():
      # creates the log file and adds the first line
-    logger = os.path.join(location, "Log.txt")
+    logger = os.path.join(location, "log.txt")
     with open(logger, "w+") as handle:
         print(str(time.ctime()) + " >>> Start", file=handle)
 
 def log(message):
     # appends to log file
-    logger = os.path.join(location, "Log.txt")
+    logger = os.path.join(location, "log.txt")
     with open(logger, "a") as handle:
         print(str(time.ctime()) + " >>> " + str(message), file=handle)
 
