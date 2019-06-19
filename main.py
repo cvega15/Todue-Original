cc834cf13b792a200063966e279547c02cee997e
dudelo <carlovega20@gmail.com>
2019-06-19 08:23:03 -0700

    gui stuff

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logger
 import utils
 from datetime import datetime
 import sys
-from PyQt5.QtWidgets import (QLineEdit, QFrame, QLabel, QSlider, QGridLayout, QPushButton, QVBoxLayout, QHBoxLayout, QApplication, QWidget, QGroupBox, QScrollArea, QSizePolicy)
+from PyQt5.QtWidgets import (QDialog, QLineEdit, QFrame, QLabel, QSlider, QGridLayout, QPushButton, QVBoxLayout, QHBoxLayout, QApplication, QWidget, QGroupBox, QScrollArea, QSizePolicy)
 from PyQt5.QtCore import Qt
 import os
 
@@ -16,53 +16,37 @@ print('░░░░░░░░░▄░░░░░░░░░░░░░░�
 print("Oh boy i worked really hard on this, i can't wait to see it run without any bugs! :D")  # !!!!VERY IMPORTANT!!!!: I added a doge meme          <------    XDDDDDDDDDDDDDDDDDDDDDDD
 
 
-class Window(QWidget):
+class App(QWidget):
 
     def __init__(self):
-        super(Window, self).__init__()
-
+        super(App, self).__init__()
         self.setWindowTitle('to due')
         self.setGeometry(300, 200, 600, 600)
         self.post_number = 0
-
         self.init_gui()
 
     def init_gui(self):
-
-        #create layout and add stuff
         self.create_task_area()
         self.vertical_layout = QVBoxLayout(self)
         self.vertical_layout.addLayout(self.create_header())
         self.vertical_layout.addWidget(self.tasks_area)
-
-        #show the window
         self.show()
 
-    def button_click(self):
-
-        sender = self.sender()
-        if sender.text() == "+":
-
-            self.tasks_layout.addWidget(Task())
-            print('task added')
+    def add_button_click(self):
+        Task_Add_Box()
         
+    def add_task(self):
+        self.tasks_layout.addWidget(Task())
+        print('task added')
+
     def create_header(self):
-
-        #create horizontal layout
         header_layout = QHBoxLayout()
-
-        #add task button
         btn_add_task = QPushButton('+')
         btn_add_task.setFixedSize(50, 50)
-        btn_add_task.clicked.connect(self.button_click)
-
-        #create text next to the add task button
+        btn_add_task.clicked.connect(self.add_button_click)
         task_label = QLabel('Add Task')
-
-        #add widgets to the header layout
         header_layout.addWidget(btn_add_task)
         header_layout.addWidget(task_label)
-
         return header_layout
 
     def create_task_area(self):
@@ -76,12 +60,40 @@ class Window(QWidget):
         self.tasks_layout = QVBoxLayout(widget)
         self.tasks_layout.addStretch(1)
 
-class Task(QWidget):
+class Task_Add_Box(QDialog):
+
+    def __init__(self, parent=None):
+        super(Task_Add_Box, self).__init__()
+
+        self.adder = QDialog()
+        self.adder.setGeometry(50, 50, 250, 200)
+        main_layout = QHBoxLayout()
+        self.adder.setLayout(main_layout)
+        button_ok = QPushButton("Add")
+        button_ok.clicked.connect(self.ok_press)
+        button_close = QPushButton("Cancel")
+        button_close.clicked.connect(self.cancel_press)
+        main_layout.addWidget(button_ok)
+        main_layout.addWidget(button_close)
+        self.adder.addAction
+        self.adder.setWindowTitle("Add a Task")
+        self.adder.exec_()
+
+    def ok_press(self):
+        
+        self.adder.reject()
+
+    def cancel_press(self):
+        print('cancel pressed')
+        self.adder.reject()
+
+class Task(QFrame):
 
     def __init__(self, parent=None):
         super(Task, self).__init__()
 
-        self.setStyleSheet('border: 1px solid red')
+        self.setFrameStyle(1)
+
         self.main_layout = QHBoxLayout()
 
         #create the left part of the task, this will be a horizontal layour with the name and the date
@@ -121,10 +133,9 @@ class Task(QWidget):
 
 def run():
     application = QApplication(sys.argv)
-    le_window = Window()
+    le_window = App()
     sys.exit(application.exec_())
 run()
-
 
 def run_program(saved=""):
 
@@ -177,7 +188,6 @@ def run_program(saved=""):
             break
 
     logger.log("End")
-
 
 # saver check
 save_location = os.path.dirname(os.path.abspath(__file__))
